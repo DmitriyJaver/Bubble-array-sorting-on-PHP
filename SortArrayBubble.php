9d3268f5ffcf928f2acfd4ecd3d938d49d502244
Dima Browun <dimontius1@gmail.com>
2019-12-26 12:50:57 +0200

    Create

---
+++ SortArrayBubble.php
@@ -0,0 +1,35 @@
+<?php
+
+        for ($i=0; $i<10; $i++){
+
+            $arr[$i] = rand(10, 100);
+        }
+
+    print_r($arr);
+
+        $countIteration = 0;
+
+        for ($i=0; $i < count($arr)-1 ; $i++){
+
+            $continueFlag = false;
+
+            for ($j=0 ; $j <count($arr)-$i-1 ; $j++){
+
+                $countIteration++;
+
+                $elem1 = $arr[$j];
+                $elem2 = $arr[$j+1];
+
+                if ($elem1 > $elem2){
+                    $arr[$j] = $elem2;
+                    $arr[$j+1] = $elem1;
+
+                    $continueFlag = true;
+                }
+            }
+            if ($continueFlag == false) {break;}
+        }
+
+print_r($arr);
+        print_r("\n number of iteration is: ". $countIteration);
+
